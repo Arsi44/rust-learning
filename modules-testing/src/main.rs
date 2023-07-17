e7bca3cad663b87447730b513c507fff78be3119
Arsi44 <gjenferd23@gmail.com>
2023-07-17 14:17:29 +0300

    add tests for modules-testing

--- modules-testing/src/main.rs
+++ modules-testing/src/main.rs
@@ -10,3 +10,18 @@ fn main() {
     println!("{}", bedroom2::light_is_on());
     println!("{}", house::HOUSE_NUMBER);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;  // Import all known names for outer area
+
+    #[test]
+    fn check_equality() {
+        assert_eq!(bedroom1::light_is_on(), false);
+    }
+
+    #[test]
+    fn check_bound() {
+        assert_eq!(bedroom1::is_neighbour_light_on(), true);
+    }
+}
